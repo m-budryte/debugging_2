6ce06dfb9dcdc44c53c3808fe848564857a49503
masha-b <mashabudryte@gmail.com>
2019-03-20 13:06:46 +0000

    workshop finished

---
+++ lib/app.rb
@@ -0,0 +1,22 @@
+require 'sinatra/base'
+require_relative "./struggle_table_flipper"
+require_relative "./random_happy_emoji"
+
+class StruggleTableFlipperApp < Sinatra::Base
+  get "/" do
+    erb(:index)
+  end
+
+  get "/emoji" do
+    erb :index, { locals: { emoji: RandomHappyEmoji.new } }
+  end
+
+  post "/flipped_struggle" do
+    p params
+    flipped_struggle = StruggleTableFlipper.new(params[:struggle]).flipped_struggle
+
+    erb :flipped_struggle, { locals: { flipped_struggle: flipped_struggle } }
+  end
+
+  run! if app_file == $0
+end
